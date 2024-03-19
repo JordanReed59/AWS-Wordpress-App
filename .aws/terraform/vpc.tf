55afe6ca01dbb0877ee3b7cb24057ceea74d94ca
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-19 16:26:23 -0400

    create and associate public route table

--- .aws/terraform/vpc.tf
+++ .aws/terraform/vpc.tf
@@ -35,3 +35,20 @@ resource "aws_subnet" "private_db_subnets" {
  availability_zone = element(var.azs, count.index)
  tags = merge(module.namespace.tags, {Name = "Private DatabaseSubnet ${count.index + 1}"})
 }
+
+# Route Tables
+resource "aws_route_table" "publc_rt" {
+ vpc_id = aws_vpc.wordpress_vpc.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gw.id
+ }
+ tags = merge(module.namespace.tags, {Name = "Public Route Table"})
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_sn_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.publc_rt.id
+}
