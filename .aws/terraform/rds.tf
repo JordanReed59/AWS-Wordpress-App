9b1ef25b62d3b7e7486aad2f87c447468f9712a2
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-21 03:08:41 -0400

    redeploy rds

--- .aws/terraform/rds.tf
+++ .aws/terraform/rds.tf
@@ -5,24 +5,24 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 # RDS instance
-# resource "aws_db_instance" "wordpress_db" {
-#   allocated_storage    = 20
-#   db_name              = "WordpressDB"
-#   identifier           = "wordpress"
-#   engine               = "mysql"
-#   engine_version       = "8.0.35"
-#   instance_class       = "db.t3.micro"
-#   username             = var.db_username
-#   password             = var.db_password
-#   parameter_group_name = "default.mysql8.0"
-#   skip_final_snapshot  = true
-#   apply_immediately    = true
-#   availability_zone    = "us-east-1b"
-#   db_subnet_group_name = aws_db_subnet_group.db_subnets.name
-#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-#   tags = merge(module.namespace.tags, {Name = "WordpressDB"})
-# #   multi_az             = true
-# }
+resource "aws_db_instance" "wordpress_db" {
+  allocated_storage    = 20
+  db_name              = "WordpressDB"
+  identifier           = "wordpress"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  apply_immediately    = true
+  availability_zone    = "us-east-1b"
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  tags = merge(module.namespace.tags, {Name = "WordpressDB"})
+#   multi_az             = true
+}
 
 # Security group for ec2 access
 resource "aws_security_group" "rds_sg" {
