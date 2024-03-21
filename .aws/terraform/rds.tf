7c49169419f3b4bcabed2a9fabdd8c371fbcf43a
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-21 01:31:49 -0400

    recreate db in vpc

--- .aws/terraform/rds.tf
+++ .aws/terraform/rds.tf
@@ -5,18 +5,18 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 # RDS instance
-# resource "aws_db_instance" "wordpress_db" {
-#   allocated_storage    = 20
-#   db_name              = "WordpressDB"
-#   engine               = "mysql"
-#   engine_version       = "8.0.35"
-#   instance_class       = "db.t3.micro"
-#   username             = var.db_username
-#   password             = var.db_password
-#   parameter_group_name = "default.mysql8.0"
-#   skip_final_snapshot  = true
-#   apply_immediately    = true
-#   availability_zone    = "us-east-1b"
-#   db_subnet_group_name = aws_db_subnet_group.db_subnets.name
-# #   multi_az             = true
-# }
+resource "aws_db_instance" "wordpress_db" {
+  allocated_storage    = 20
+  db_name              = "WordpressDB"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  apply_immediately    = true
+  availability_zone    = "us-east-1b"
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+#   multi_az             = true
+}
