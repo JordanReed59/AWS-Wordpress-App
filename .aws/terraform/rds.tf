744d6e315065ee939bf9624de6d7c99c8a8d36d6
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-21 00:58:15 -0400

    update subnet group name

--- .aws/terraform/rds.tf
+++ .aws/terraform/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnets" {
-  name       = "main"
+  name       = "db_subnet_group"
   subnet_ids = aws_subnet.private_db_subnets[*].id
   tags = merge(module.namespace.tags, {Name = "My DB subnet group"})
 }
