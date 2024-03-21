e50585ef8796f522c491ab097918a74563db825c
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-21 02:26:05 -0400

    create security groups and attach to rds

---
+++ .aws/terraform/ec2.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+  tags = merge(module.namespace.tags, {Name = "ec2-sg"})
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_sg_ssh" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = var.my_ip
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  tags = merge(module.namespace.tags, {Name = "allow-ssh"})
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_sg_ipv4_https" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = aws_vpc.wordpress_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+  tags = merge(module.namespace.tags, {Name = "allow-https"})
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_sg_ipv4_http" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = aws_vpc.wordpress_vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+  tags = merge(module.namespace.tags, {Name = "allow-http"})
+}
+
+# remove when adding instances to private subnet
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+  tags = merge(module.namespace.tags, {Name = "allow-outbound"})
+}
