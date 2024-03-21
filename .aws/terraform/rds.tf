a8947242f4644606d5ef2f6b83d9e1e255674b57
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-21 02:50:39 -0400

    redeploy rds and sg

--- .aws/terraform/rds.tf
+++ .aws/terraform/rds.tf
@@ -5,37 +5,37 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 # RDS instance
-# resource "aws_db_instance" "wordpress_db" {
-#   allocated_storage    = 20
-#   db_name              = "WordpressDB"
-#   engine               = "mysql"
-#   engine_version       = "8.0.35"
-#   instance_class       = "db.t3.micro"
-#   username             = var.db_username
-#   password             = var.db_password
-#   parameter_group_name = "default.mysql8.0"
-#   skip_final_snapshot  = true
-#   apply_immediately    = true
-#   availability_zone    = "us-east-1b"
-#   db_subnet_group_name = aws_db_subnet_group.db_subnets.name
-# #   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-#   tags = merge(module.namespace.tags, {Name = "WordpressDB"})
-# #   multi_az             = true
-# }
+resource "aws_db_instance" "wordpress_db" {
+  allocated_storage    = 20
+  db_name              = "WordpressDB"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  apply_immediately    = true
+  availability_zone    = "us-east-1b"
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  tags = merge(module.namespace.tags, {Name = "WordpressDB"})
+#   multi_az             = true
+}
 
 # Security group for ec2 access
-# resource "aws_security_group" "rds_sg" {
-#   name        = "rds_sg"
-#   description = "Allow MYSQL/Aurora inbound traffic"
-#   vpc_id      = aws_vpc.wordpress_vpc.id
-#   tags = merge(module.namespace.tags, {Name = "rds-sg"})
-# }
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow MYSQL/Aurora inbound traffic"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+  tags = merge(module.namespace.tags, {Name = "rds-sg"})
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "rds_sg_mysql_access" {
-#   security_group_id            = aws_security_group.rds_sg.id
-#   referenced_security_group_id = aws_security_group.ec2_sg.id
-#   from_port                    = 3306
-#   ip_protocol                  = "tcp"
-#   to_port                      = 3306
-#   tags = merge(module.namespace.tags, {Name = "allow-mysql"})
-# }
+resource "aws_vpc_security_group_ingress_rule" "rds_sg_mysql_access" {
+  security_group_id            = aws_security_group.rds_sg.id
+  referenced_security_group_id = aws_security_group.ec2_sg.id
+  from_port                    = 3306
+  ip_protocol                  = "tcp"
+  to_port                      = 3306
+  tags = merge(module.namespace.tags, {Name = "allow-mysql"})
+}
