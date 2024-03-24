58aca153fa3a215493b156685bfd51c824d55585
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-24 10:59:46 -0400

    acl

--- .aws/terraform/s3.tf
+++ .aws/terraform/s3.tf
@@ -35,13 +35,20 @@ data "aws_iam_policy_document" "allow_cloudfront_and_public_access" {
 }
 
 # public bucket config
-resource "aws_s3_bucket_public_access_block" "disable_block" {
+resource "aws_s3_bucket_public_access_block" "enable_block" {
   bucket = aws_s3_bucket.media_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.media_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 # resource "aws_s3_bucket_acl" "bucket_acl" {
