fb71ea23b850f91886765f983783da72641cf861
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-24 11:25:33 -0400

    block public access to s3

--- .aws/terraform/s3.tf
+++ .aws/terraform/s3.tf
@@ -39,8 +39,8 @@ resource "aws_s3_bucket_public_access_block" "enable_block" {
   bucket = aws_s3_bucket.media_bucket.id
 
   block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = true
+  block_public_policy     = true
+  ignore_public_acls      = false
   restrict_public_buckets = true
 }
 
