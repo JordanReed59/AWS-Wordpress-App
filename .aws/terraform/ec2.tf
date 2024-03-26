01491b179f8a56ac99eabe1980aa362e837f47cd
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-26 10:30:06 -0400

    increase grace period

--- .aws/terraform/ec2.tf
+++ .aws/terraform/ec2.tf
@@ -179,7 +179,7 @@ resource "aws_autoscaling_group" "my_asg" {
   min_size           = 1
   vpc_zone_identifier  = aws_subnet.private_compute_subnets[*].id
   health_check_type = "EC2"
-  health_check_grace_period = 300 # default is 300 seconds  
+  health_check_grace_period = 900 # default is 300 seconds  
   # Launch Template
   launch_template {
     id      = aws_launch_template.bastion_launch_template.id
