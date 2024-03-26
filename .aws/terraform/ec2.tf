c28dcd16937d91d8cfe84cdb17cf2c45b89ed524
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-26 12:13:58 -0400

    update ip and deploy asg

--- .aws/terraform/ec2.tf
+++ .aws/terraform/ec2.tf
@@ -173,26 +173,26 @@ resource "aws_launch_template" "bastion_launch_template" {
 }
 
 # Autoscaling Group
-# resource "aws_autoscaling_group" "my_asg" {
-#   name_prefix = "bastionasg-"
-#   desired_capacity   = 1
-#   max_size           = 1
-#   min_size           = 1
-#   vpc_zone_identifier  = aws_subnet.private_compute_subnets[*].id
-#   health_check_type = "EC2"
-#   health_check_grace_period = 300 # default is 300 seconds  
-#   # Launch Template
-#   launch_template {
-#     id      = aws_launch_template.bastion_launch_template.id
-#     version = aws_launch_template.bastion_launch_template.latest_version
-#   }
-#   # Instance Refresh
-#   instance_refresh {
-#     strategy = "Rolling"
-#     preferences {
-#       #instance_warmup = 300 # Default behavior is to use the Auto Scaling Group's health check grace period.
-#       min_healthy_percentage = 50
-#     }
-#     triggers = [ "desired_capacity" ] # You can add any argument from ASG here, if those has changes, ASG Instance Refresh will trigger
-#   }       
-# }
+resource "aws_autoscaling_group" "my_asg" {
+  name_prefix = "bastionasg-"
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  vpc_zone_identifier  = aws_subnet.private_compute_subnets[*].id
+  health_check_type = "EC2"
+  health_check_grace_period = 300 # default is 300 seconds  
+  # Launch Template
+  launch_template {
+    id      = aws_launch_template.bastion_launch_template.id
+    version = aws_launch_template.bastion_launch_template.latest_version
+  }
+  # Instance Refresh
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      #instance_warmup = 300 # Default behavior is to use the Auto Scaling Group's health check grace period.
+      min_healthy_percentage = 50
+    }
+    triggers = [ "desired_capacity" ] # You can add any argument from ASG here, if those has changes, ASG Instance Refresh will trigger
+  }       
+}
