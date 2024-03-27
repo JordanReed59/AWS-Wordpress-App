c66366bd129a2298fcfa34bdfc804cd4d0c0c4b5
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-27 11:53:33 -0400

    add health check

--- .aws/terraform/alb.tf
+++ .aws/terraform/alb.tf
@@ -31,18 +31,15 @@ resource "aws_lb_target_group" "wordpress_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.wordpress_vpc.id
   health_check {
-    enabled = false
+    enabled             = true
+    port                = 80  # Use port 80 for health checks
+    interval            = 300
+    protocol            = "HTTP"
+    path                = "/"  # Use the root path for health checks
+    matcher             = "200"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
   }
-  # health_check {
-  #   enabled             = true
-  #   port                = 80  # Use port 80 for health checks
-  #   interval            = 30
-  #   protocol            = "HTTP"
-  #   path                = "/"  # Use the root path for health checks
-  #   matcher             = "200"
-  #   healthy_threshold   = 3
-  #   unhealthy_threshold = 3
-  # }
   tags = module.namespace.tags
 }
 
