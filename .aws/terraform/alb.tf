70cf623e7c10a7f0b456328d81e0e468692d424b
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-28 07:52:13 -0400

    update path for tg

--- .aws/terraform/alb.tf
+++ .aws/terraform/alb.tf
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "wordpress_tg" {
     port                = 80  # Use port 80 for health checks
     interval            = 30
     protocol            = "HTTP"
-    path                = "/healthcheck.html"  # Use the root path for health checks
+    path                = "/"  # Use the root path for health checks
     matcher             = "200"
     healthy_threshold   = 3
     unhealthy_threshold = 3
